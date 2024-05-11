d9b48e45236513f35f9e14a4a8f0d74ef509e4b1
muzahid120 <muzahidsk771@gmail.com>
2024-05-11 07:58:26 +0530

    add gridsearchcv

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import pickle
 from src.exception import CustomException
 from src.logger import logging
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,obj):
@@ -26,11 +27,20 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise CustomException(sys,e)
-def evalute_model(x_train,x_test,y_train,y_test,models):
+def evalute_model(x_train,x_test,y_train,y_test,models,param):
     try:
         report={}
         for i in range (len(list(models))):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            
+
+            model.set_params(**gs.best_params_)
+            
             model.fit(x_train,y_train) # model Train
 
             y_train_pred=model.predict(x_train)
